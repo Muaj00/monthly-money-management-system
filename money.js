34668cbf5c4ac984355d375a25b5c8624eeef145
Muaj00 <muaj15-14389@diu.edu.bd>
2023-05-03 22:10:30 +0600

    vanila js done

---
+++ money.js
@@ -0,0 +1,60 @@
+document.getElementById('calculate-button').addEventListener('click',function(){
+    
+   const inputIncome =  document.getElementById('income');
+   const textIncome = inputIncome.value;
+   const income = parseFloat(textIncome);
+   
+
+   const inputfood =  document.getElementById('food');
+   const textFoodaAmount = inputfood.value;
+   const foodAmount = parseFloat(textFoodaAmount);
+    
+   
+
+   const inputrent =  document.getElementById('rent');
+   const textRentAmount = inputrent.value;
+   const rentAmount = parseFloat(textRentAmount);
+   
+
+   const inputclothes =  document.getElementById('clothes');
+   const textclothesAmount = inputclothes.value;
+   const clothesAmount = parseFloat(textclothesAmount);
+
+   const expenses = foodAmount + rentAmount + clothesAmount;
+
+
+   const totalExpenses = document.getElementById('total-expenses');
+   const totalExpensesInText = totalExpenses.innerText;
+   totalExpenses.innerText = expenses;
+
+   const balance = income - expenses;
+
+   const previusBalance = document.getElementById('balance');
+   const totalBalanceText = previusBalance.innerText;
+   previusBalance.innerText = balance;
+
+
+   document.getElementById('save-button').addEventListener('click',function(){
+
+    const inputPercent = document.getElementById('parcentage-input');
+    const inputPercentText = inputPercent.value;
+    const percentageInput = parseFloat(inputPercentText);
+
+    const savingAmount = (balance) * (percentageInput/100);
+
+    const finalAmount = document.getElementById('final-saving-amount');
+    const finalAmountText = finalAmount.innerText;
+    finalAmount.innerText = savingAmount;
+
+    const totalRemainingBalance = balance - savingAmount;
+
+    const remainingBalance = document.getElementById('final-balance');
+    const remainingBalanceText = remainingBalance.innerText;
+    remainingBalance.innerText = totalRemainingBalance;
+
+    });
+
+
+   
+});
+
