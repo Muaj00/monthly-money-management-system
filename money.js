adbec18487315a2d0ce500fb3b7a120b4039f77c
Muaj00 <muaj15-14389@diu.edu.bd>
2023-05-04 02:17:25 +0600

    problem solved

--- money.js
+++ money.js
@@ -1,65 +1,85 @@
+function commonCalculation(inputId){
+    const inputIncome =  document.getElementById(inputId);
+    const textIncome = inputIncome.value;
+    const income = parseFloat(textIncome);
+    inputIncome.value = '';
+    return income;
+}
+
+function commonText(text,expenses){
+    const totalExpenses = document.getElementById(text);
+    const totalExpensesInText = totalExpenses.innerText;
+    totalExpenses.innerText = expenses;
+}
+//common function ends
+
 document.getElementById('calculate-button').addEventListener('click',function(){
     
-   const inputIncome =  document.getElementById('income');
-   const textIncome = inputIncome.value;
-   const income = parseFloat(textIncome);
-   inputIncome.value = '';
+//  calling functions and calculate total expenses
+    const income = commonCalculation('income');
+   
+    const foodAmount = commonCalculation('food');
+
+    const rentAmount = commonCalculation('rent');
+   
+    const clothesAmount = commonCalculation('clothes');
+
+    if(income>0 && foodAmount>0 && rentAmount>0 && clothesAmount>0)
+    {
+        const expenses = foodAmount + rentAmount + clothesAmount;
+
+
+        //    calling function to calculate the expenses
+                commonText('total-expenses',expenses);
+        
+                 const balance = income - expenses;
+        
+        //    calling function to calculate the balance
+                commonText('balance',balance);
+
+               
+                    document.getElementById('save-button').addEventListener('click',function(){
+                        if (balance > expenses)
+                        {
+                        const inputPercent = document.getElementById('parcentage-input');
+                        const inputPercentText = inputPercent.value;
+                        const percentageInput = parseFloat(inputPercentText);
+                    
+                        const savingAmount = (balance) * (percentageInput/100);
+                    
+                        const finalAmount = document.getElementById('final-saving-amount');
+                        const finalAmountText = finalAmount.innerText;
+                        finalAmount.innerText = savingAmount;
+                    
+                        const totalRemainingBalance = balance - savingAmount;
+                    
+                        const remainingBalance = document.getElementById('final-balance');
+                        const remainingBalanceText = remainingBalance.innerText;
+                        remainingBalance.innerText = totalRemainingBalance;
+                    }
+                    else
+                {
+                    window.prompt("you can't save money because of your low balance than expenses");
+                }
+
+            });
+                
+
+                
+    }
+    else
+    {
+
+        window.prompt("Enter valid number");
+    }
+
    
 
-   const inputfood =  document.getElementById('food');
-   const textFoodaAmount = inputfood.value;
-   const foodAmount = parseFloat(textFoodaAmount);
-   inputfood.value = '';
-    
+
+  
+
    
-
-   const inputrent =  document.getElementById('rent');
-   const textRentAmount = inputrent.value;
-   const rentAmount = parseFloat(textRentAmount);
-   inputrent.value = '';
-   
-
-   const inputclothes =  document.getElementById('clothes');
-   const textclothesAmount = inputclothes.value;
-   const clothesAmount = parseFloat(textclothesAmount);
-   inputclothes.value = '';
-
-   const expenses = foodAmount + rentAmount + clothesAmount;
-
-
-   const totalExpenses = document.getElementById('total-expenses');
-   const totalExpensesInText = totalExpenses.innerText;
-   totalExpenses.innerText = expenses;
-
-   const balance = income - expenses;
-
-   const previusBalance = document.getElementById('balance');
-   const totalBalanceText = previusBalance.innerText;
-   previusBalance.innerText = balance;
-
-
-   document.getElementById('save-button').addEventListener('click',function(){
-
-    const inputPercent = document.getElementById('parcentage-input');
-    const inputPercentText = inputPercent.value;
-    const percentageInput = parseFloat(inputPercentText);
-    inputPercent.value = '';
-
-    const savingAmount = (balance) * (percentageInput/100);
-
-    const finalAmount = document.getElementById('final-saving-amount');
-    const finalAmountText = finalAmount.innerText;
-    finalAmount.innerText = savingAmount;
-
-    const totalRemainingBalance = balance - savingAmount;
-
-    const remainingBalance = document.getElementById('final-balance');
-    const remainingBalanceText = remainingBalance.innerText;
-    remainingBalance.innerText = totalRemainingBalance;
-
-    });
-
-
+  
    
 });
 
